9e712e5077990ee7245cb284bd26c5819cb27f02
T-R-E-E <Arvindsb618@gmail.com>
2023-08-19 11:20:35 -0400

    added group name input

--- popup.js
+++ popup.js
@@ -8,47 +8,12 @@ chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
 
   // Query for all tabs in the current window.
   chrome.tabs.query({ currentWindow: true }, function(allTabs) {
-    // Filter the tabs to include only those with an index greater than the active tab's index.
-    tabsToRight = allTabs.filter(tab => tab.index > activeTabIndex);
-
-    // Now that 'tabsToRight' has been populated, you can proceed to use it.
-
-    // Fetch an HTML template element with the id "li_template."
-    const template = document.getElementById("li_template");
-
-    // Create a set to store elements that will be appended to the DOM.
-    const elements = new Set();
-
-    // Iterate through the 'tabsToRight' array.
-    for (const tab of tabsToRight) {
-      
-      // Clone the template element to create a new list item.
-      const element = template.content.firstElementChild.cloneNode(true);
-
-      // Extract the title and pathname information from the tab.
-      const title = tab.title.split("-")[0].trim();
-      const pathname = new URL(tab.url).pathname.slice("/docs".length);
-
-      // Update the cloned element with tab information.
-      element.querySelector(".title").textContent = title;
-      element.querySelector(".pathname").textContent = pathname;
-
-      // Add a click event listener to the anchor tag within the list item.
-      element.querySelector("a").addEventListener("click", async () => {
-        // Set the clicked tab as active and focus its window.
-        await chrome.tabs.update(tab.id, { active: true });
-        await chrome.windows.update(tab.windowId, { focused: true });
-      });
-
-      // Add the cloned element to the 'elements' set.
-      elements.add(element);
-    }
-
-    // Append the elements to the unordered list in the DOM.
-    document.querySelector("ul").append(...elements);
+    // Filter the tabs to include only those with an index greater than OR equal to the active tab's index.
+    tabsToRight = allTabs.filter(tab => tab.index >= activeTabIndex);
 
     // Fetch the button element.
     const button = document.querySelector("button");
+
 
     // Add a click event listener to the button.
     button.addEventListener("click", async () => {
@@ -58,8 +23,8 @@ chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
       // Group the selected tabs into a new tab group.
       const group = await chrome.tabs.group({ tabIds });
 
-      // Update the title of the created tab group to "DOCS."
-      await chrome.tabGroups.update(group, { title: "DOCS" });
+      // Update the title of the created tab group to whatever the name input is
+      await chrome.tabGroups.update(group, { title: document.getElementById("name").value});
     });
   });
 });
